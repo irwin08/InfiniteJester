eee85c046d99d23fa7458b54540de3f451e9cbf9
Jesse Irwin <jesse.irwin11@gmail.com>
2022-05-15 10:35:37 -0700

    Configure project structure and actions

---
+++ TestInfiniteJester/TestJesterOutput.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InfiniteJester;
+
+namespace TestJesterOutput
+{
+    [TestClass]
+    public class TestJesterOutput
+    {
+        public TestJesterOutput()
+        { 
+
+        }
+
+        [TestMethod]
+        public void TestFunction()
+        {
+
+        }
+    }
+}
